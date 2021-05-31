90dac14df41761eb2c5ecb0e355aeb67498d2c11
FOO JING XIANG <20011262@myrp.edu.sg>
2021-05-31 19:12:53 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20011262/deomocontextmenutranslation2/MainActivity.java
@@ -0,0 +1,45 @@
+package sg.edu.rp.c346.id20011262.deomocontextmenutranslation2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvTranslatedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+        registerForContextMenu(tvTranslatedText);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.EnglishSelection ) {
+            tvTranslatedText.setText("Hello");
+            return true;
+        } else if(id == R.id.ItalianSelection) {
+            tvTranslatedText.setText("Ciao");
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+}
